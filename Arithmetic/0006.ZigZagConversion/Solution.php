0d6377a35b70a61d655fcf7b52ef377a928f93b8
Anthony <Anthony@hthui.com>
2019-05-09 15:00:42 +0800

    add 0006

---
+++ Arithmetic/0006.ZigZagConversion/Solution.php
@@ -0,0 +1,41 @@
+<?php
+
+$s = 'LEETCODEISHIRING';
+$numRows = 3;
+$answer = solute($s, $numRows);
+echo $answer;
+function solute($s, $numRows)
+{
+    for($i = 0; $i < $numRows; $i ++){
+        $data[$i] = '';
+    }
+    $currentRow = 0;
+    $to = 0;
+    for($i = 0; $i < strlen($s); $i ++){
+        $data[$currentRow] .= $s[$i];
+        if($to == 0){
+            if($currentRow < $numRows - 1){
+                $currentRow ++;
+            }else{
+                $currentRow --;
+                $to = 1;
+            }
+        }else{
+            if($currentRow > 0){
+                $currentRow --;
+            }else{
+                $currentRow ++;
+                $to = 0;
+            }
+        }
+        
+    }
+
+    $answer = '';
+    for($i = 0; $i < $numRows; $i ++){
+        $answer .= $data[$i];
+    }
+
+    return $answer;
+}
+
---
+++ Arithmetic/0006.ZigZagConversion/readme.md
@@ -0,0 +1,11 @@
+# 题目
+
+将一个给定字符串根据给定的行数，以从上往下、从左到右进行 Z 字形排列。
+
+# 分析
+
+第一感觉就很简单,只要遍历一遍字符串就好,时间复杂度O(n),空间复杂度O(n)。
+
+用PHP写了下解答,本地运行正确,但是提交失败,报了以下错误,然而我代码根本没45行,我也是无语了
+
+Line 45: PHP Parse error:  syntax error, unexpected '$ser' (T_VARIABLE), expecting function (T_FUNCTION) or const (T_CONST) in solution.php
